33c79770a9b45471a2768e59184e36345a344c9a
jayjaybunce <jayjaybunce@gmail.com>
2019-11-12 12:31:14 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay =[];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cardOne = cards[2];
+var cardTwo = cards[1];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("Use flipped " +cardOne+ " and " + cardTwo);
+
+if (cardsInPlay.length === 2){
+	console.log("Two cards have been played");
+}	if(cardOne === cardTwo){
+		alert("You've found a match");
+}	else{
+		alert("Sorry, try again!");
+}
