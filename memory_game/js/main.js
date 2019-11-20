272e7e847220278d671018a5dbebb7ceb09c4f1b
Janson Bunce <Razumikin@Jansons-MacBook-Pro.local>
2019-11-20 02:18:12 -0700

    fixed problem with card shuffling and duplicate cards being present in game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,16 +24,7 @@ var cards = [
 var score = 0;
 var cardsInPlay =[];
 
-function flipCard(){
 
-	var cardId = this.getAttribute('data-id');
-	cardsInPlay.push(cards[cardId].rank);
-	console.log("User flipped " + cards[cardId].rank +" of " + cards[cardId].suit + " located at " + "'" +cards[cardId].cardImage+"'");
-	this.setAttribute('src',cards[cardId].cardImage);
-	if(cardsInPlay.length === 2){
-	checkForMatch(cardId);
-	};
-}
 
 function checkForMatch(cardId){
 	if(cardsInPlay[0] === cardsInPlay[1]){
@@ -55,36 +46,52 @@ function getRandomInt(max) {
 
 
 function createBoard(){
+	let numbers = shuffle();
 	for(let i = 0; i < cards.length ;i++){
 		let cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png');
-		let randomNum = getRandomInt(4);
-		cardElement.setAttribute('data-id',randomNum);
+		// let randomNum = getRandomInt(4);
+		cardElement.setAttribute('data-id',numbers[i]);
 		cardElement.addEventListener('click',flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
-
-
 	}
 }
 
 //Add reset function - resets cardsInPlay array and 'src' attribute of selevted game pieces.
 
+function shuffle() {
+    let numbers = [0,1,2,3]
+    for(var j, x, i = numbers.length; i; j = parseInt(Math.random() * i), x = numbers[--i], numbers[i] = numbers[j], numbers[j] = x);
+    return numbers;
+};
+
+
+
+
 function resetBoard(){
+	let numbers = shuffle();
 	for(let i =0;i < cards.length;i++){
 	let cardElement = document.getElementsByTagName('img')[i];
 	cardElement.setAttribute('src','images/back.png');
-	let randomNum = getRandomInt(4);
-	let resetCard = document.getElementsByTagName('img')[0];
-	resetCard.setAttribute('data-id',randomNum);
-	if(i === 3){
-
-		
+	let resetCard = document.getElementsByTagName('img')[i];
+	resetCard.setAttribute('data-id',numbers[i]);
+	console.log(numbers[i])
+	if(i === 3){		
 		return cardsInPlay = [];
 	}
 
 	};
 };
 
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank +" of " + cards[cardId].suit + " located at " + "'" +cards[cardId].cardImage+"'");
+	this.setAttribute('src',cards[cardId].cardImage);
+	if(cardsInPlay.length === 2){
+	checkForMatch(cardId);
+	};
+}
 
 
 
